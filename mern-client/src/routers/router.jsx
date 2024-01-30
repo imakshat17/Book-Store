212c449faecb0cebd56aa128214c3e095c2375bf
imakshat17 <imakshat17@gmail.com>
2024-01-30 11:31:15 +0530

    Modified Signup to SignUp component

--- mern-client/src/routers/router.jsx
+++ mern-client/src/routers/router.jsx
@@ -14,7 +14,7 @@ import Dashboard from "../dashBoard/Dashboard";
 import UploadBook from "../dashBoard/UploadBook";
 import Manage from "../dashBoard/Manage";
 import Edit from "../dashBoard/Edit";
-import Signup from "../components/Signup";
+import Signup from "../components/SignUp";
 import Login from "../components/Login";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
 import Logout from "../components/Logout";
@@ -77,7 +77,7 @@ import Logout from "../components/Logout";
     },
     {
       path:"sign-up",
-      element:<Signup/>
+      element:<SignUp/>
     },{
       path:"login",
       element:<Login/>
